f6d863dc583ad3b0244c13dbca424fab9e80c288
hashus42 <hsnhsyntrkmn@gmail.com>
2024-08-09 11:50:00 +0300

    prepare usiing api

--- data_prep.py
+++ data_prep.py
@@ -2,6 +2,13 @@ import os
 import cv2
 import numpy as np
 
+from inference_sdk import InferenceHTTPClient
+
+CLIENT = InferenceHTTPClient(
+    api_url="https://detect.roboflow.com",
+    api_key="nTDVCTv5Qb4iNEzfT1cX"
+)
+
 
 def get_img_path(dir_path):
     img_path = []
@@ -15,17 +22,17 @@ def get_img_path(dir_path):
     return img_path
 
 
-def save_img(idx):
-    img = cv2.imread(img_path[idx])
+def save_img(imageId, saveId):
+    img = cv2.imread(img_path[imageId])
     img = cv2.resize(img, (WIN_W, WIN_H))
     img_save = img[drawBox[1]+2:drawBox[3]-1, drawBox[0]+2:drawBox[2]-1]
     img_save = cv2.cvtColor(img_save, cv2.COLOR_BGR2GRAY)
     img_save = cv2.resize(img_save, (100, 100))
 
-    if not os.path.exists(f"datasets/kaggle-drone/pos_images"):
-        os.makedirs(f"datasets/kaggle-drone/pos_images")
+    if not os.path.exists(f"pos_images"):
+        os.makedirs(f"pos_images")
 
-    cv2.imwrite(f"datasets/kaggle-drone/pos_images/{idx}.jpg", img_save)
+    cv2.imwrite(f"pos_images/{saveId}.jpg", img_save)
 
 
 # Mouse callback function
@@ -59,6 +66,7 @@ def draw_rectangle(event, x, y, flags, param):
                 drawBox[1] = y
                 drawBox[3] = start_y
     elif event == cv2.EVENT_LBUTTONUP:
+        print(f"drawBox: {drawBox}")
         mouseUp = True
         drawing = False
 
@@ -70,15 +78,22 @@ drawing = False
 mouseUp = False
 first = True
 drawBox = [0, 0, 0, 0]
-imageId = 0
-saveId = 0
-saveCurrId = 0
+database = []
+file = open("database.txt", "r")
+for line in file.readlines():
+    database.append(int(line))
+file.close()
+imageId = database[0]
+saveId = database[1]
+print(f"imageId: {imageId} | saveId: {saveId}")
 
 img_path = get_img_path('datasets')
 
 # Load the image
-img = cv2.imread(img_path[imageId])
+img = cv2.imread(img_path[imageId]) # 125 # 140
 img = cv2.resize(img, (WIN_W, WIN_H))
+cv2.imwrite(img_path[imageId], img)
+print(img.shape)
 
 # Create a window and display the image
 cv2.imshow('Image', img)
@@ -103,23 +118,63 @@ while True:
     if key == 27:  # 'esc' key
         break
     elif key == ord('d'):  # 'd' key
-        save_img(imageId)
+        if mouseUp:
+            save_img(imageId, saveId)
         if imageId < len(img_path) - 1:
             imageId += 1
             saveId += 1
-            saveCurrId = 1
-            mouseUp = False
+            img = cv2.imread(img_path[imageId])  # 125 # 140
+            img = cv2.resize(img, (WIN_W, WIN_H))
+            cv2.imwrite(img_path[imageId], img)
+            print(img_path[imageId])
+            try:
+                result = CLIENT.infer(img_path[imageId], model_id="drone-detection-a1tsf/6")
+                drawBox = list(result["predictions"][0].values())[:4]
+                drawBox = [int(i) for i in drawBox]
+                drawBox[0] = int(drawBox[0] - drawBox[2] / 2)
+                drawBox[1] = int(drawBox[1] - drawBox[3] / 2)
+                drawBox[2] = drawBox[0] + drawBox[2]
+                drawBox[3] = drawBox[1] + drawBox[3]
+                print(f"drawBox: {drawBox}")
+                mouseUp = True
+            except Exception as e:
+                print(e)
+        print(f"ImageId: {imageId} | SaveId: {saveId}")
     elif key == ord('a'):
-        save_img(imageId)
+        if mouseUp:
+            save_img(imageId, saveId)
         if imageId > 0:
             imageId -= 1
-            saveId -= saveCurrId
+            saveId -= 1
             saveCurrId = 0
             mouseUp = False
+        print(f"ImageId: {imageId} | SaveId: {saveId}")
     elif key == ord(' '):
-        save_img(saveId)
+        if mouseUp:
+            save_img(imageId, saveId)
         saveId += 1
-        saveCurrId += 1
-        mouseUp = False
+        print(f"ImageId: {imageId} | SaveId: {saveId}")
+    elif key == ord("w"):
+        img = cv2.imread(img_path[imageId])  # 125 # 140
+        img = cv2.resize(img, (WIN_W, WIN_H))
+        cv2.imwrite(img_path[imageId], img)
+        print(img_path[imageId])
+        try:
+            result = CLIENT.infer(img_path[imageId], model_id="drone-detection-a1tsf/6")
+            drawBox = list(result["predictions"][0].values())[:4]
+            drawBox = [int(i) for i in drawBox]
+            drawBox[0] = int(drawBox[0] - drawBox[2] / 2)
+            drawBox[1] = int(drawBox[1] - drawBox[3] / 2)
+            drawBox[2] = drawBox[0] + drawBox[2]
+            drawBox[3] = drawBox[1] + drawBox[3]
+            print(f"drawBox: {drawBox}")
+            mouseUp = True
+        except Exception as e:
+            print(e)
+
+file = open("database.txt", "w")
+file.write(str(imageId) + "\n")
+file.write(str(saveId))
+file.close()
 # Close the window
 cv2.destroyAllWindows()
